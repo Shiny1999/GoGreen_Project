cdf4c4e94c3cf5770170c279274b9a46c77aa7bd
Ckkk112138 <72601184+Ckkk112138@users.noreply.github.com>
2023-10-27 16:44:40 +0200

    Add NLP models and execution scripts

--- examples/linux-powerjoular-profiling/RunnerConfig.py
+++ examples/linux-powerjoular-profiling/RunnerConfig.py
@@ -22,7 +22,7 @@ class RunnerConfig:
 
     # ================================ USER SPECIFIC CONFIG ================================
     """The name of the experiment."""
-    name:                       str             = "new_runner_experiment"
+    name:                       str             = "gpt_runner_experiment_2"
 
     """The path in which Experiment Runner will create a folder with the name `self.name`, in order to store the
     results from this experiment. (Path does not need to exist - it will be created if necessary.)
@@ -34,7 +34,7 @@ class RunnerConfig:
 
     """The time Experiment Runner will wait after a run completes.
     This can be essential to accommodate for cooldown periods on some systems."""
-    time_between_runs_in_ms:    int             = 1000
+    time_between_runs_in_ms:    int             = 300000
 
     # Dynamic configurations can be one-time satisfied here before the program takes the config as-is
     # e.g. Setting some variable based on some criteria
@@ -58,10 +58,10 @@ class RunnerConfig:
     def create_run_table_model(self) -> RunTableModel:
         """Create and return the run_table model here. A run_table is a List (rows) of tuples (columns),
         representing each run performed"""
-        cpu_limit_factor = FactorModel("cpu_limit", [25, 50, 100])
+        run_factor = FactorModel("run", [0])
         self.run_table_model = RunTableModel(
-            factors = [cpu_limit_factor],
-            data_columns=['avg_cpu', 'total_energy']
+            factors = [run_factor],
+            data_columns=['avg_cpu', 'avg_memory', 'total_energy', 'total_inference_time']
         )
         return self.run_table_model
 
@@ -80,38 +80,59 @@ class RunnerConfig:
         For example, starting the target system to measure.
         Activities after starting the run should also be performed here."""
         
-        cpu_limit = context.run_variation['cpu_limit']
+        # cpu_limit = context.run_variation['cpu_limit']
 
         # start the target
-        self.target = subprocess.Popen(['python', './primer.py'],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=self.ROOT_DIR,
+        self.target = subprocess.Popen(['python3', './gpt-2.py'],  cwd=self.ROOT_DIR,
         )
 
         # Configure the environment based on the current variation
-        subprocess.check_call(shlex.split(f'cpulimit -b -p {self.target.pid} --limit {cpu_limit}'))
+        # subprocess.check_call(shlex.split(f'cpulimit -b -p {self.target.pid} --limit {cpu_limit}'))
         
 
     def start_measurement(self, context: RunnerContext) -> None:
         """Perform any activity required for starting measurements."""
+        
+        profiler_cmd_mem = f'ps -p {self.target.pid} --noheader -o %mem'
+        wrapper_script = f'''
+        while true; do {profiler_cmd_mem}; sleep 1; done
+        '''
+
+        
 
         profiler_cmd = f'powerjoular -l -p {self.target.pid} -f {context.run_dir / "powerjoular.csv"}'
 
         time.sleep(1) # allow the process to run a little before measuring
         self.profiler = subprocess.Popen(shlex.split(profiler_cmd))
+        # memory profiler
+        self.profiler_mem = subprocess.Popen(['sh', '-c', wrapper_script],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        
+        self.stime = time.time()
+        
 
     def interact(self, context: RunnerContext) -> None:
         """Perform any interaction with the running target system here, or block here until the target finishes."""
 
         # No interaction. We just run it for XX seconds.
         # Another example would be to wait for the target to finish, e.g. via `self.target.wait()`
-        output.console_log("Running program for 20 seconds")
-        time.sleep(20)
+        output.console_log("Running program for n seconds")
+        
+        self.target.wait()
+        output.console_log("End running")
+
 
     def stop_measurement(self, context: RunnerContext) -> None:
         """Perform any activity here required for stopping measurements."""
+        self.etime = time.time()
 
         os.kill(self.profiler.pid, signal.SIGINT) # graceful shutdown of powerjoular
         self.profiler.wait()
+        
+        self.profiler_mem.kill()
+        self.profiler_mem.wait()
+        
 
     def stop_run(self, context: RunnerContext) -> None:
         """Perform any activity here required for stopping the run.
@@ -128,9 +149,23 @@ class RunnerConfig:
         # powerjoular.csv - Power consumption of the whole system
         # powerjoular.csv-PID.csv - Power consumption of that specific process
         df = pd.read_csv(context.run_dir / f"powerjoular.csv-{self.target.pid}.csv")
+        
+        df2 = pd.DataFrame(columns=['mem_usage'])
+        for i, l in enumerate(self.profiler_mem.stdout.readlines()):
+            mem_usage=float(l.decode('ascii').strip())
+            df2.loc[i] = [mem_usage]
+        
+        df2.to_csv(context.run_dir / 'raw_data.csv', index=False)
+        
+        df3 = pd.DataFrame(columns=['inf_time'])
+        total_time = self.etime - self.stime
+        df3.loc[0] = [total_time]
+        
         run_data = {
-            'avg_cpu': round(df['CPU Utilization'].sum(), 3),
+            'avg_cpu': round(df['CPU Utilization'].mean(), 3),
+            'avg_memory': round(df2['mem_usage'].mean(), 3),
             'total_energy': round(df['CPU Power'].sum(), 3),
+            'total_inference_time': round(df3['inf_time'][0], 3),
         }
         return run_data
 
